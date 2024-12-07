6c0a277d80f3e73643afef4b311f8ccd59a46f2b
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2024-12-07 22:33:35 +0600

    add primary nav structure

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,33 @@
+import { menuItems } from "@/config/site";
+import { FaPhoneAlt } from "react-icons/fa";
+
 const Navbar = () => {
-  return <div className="uppercase w-full bg-black">
-
-
-    
-  </div>;
+  return (
+    <div className="uppercase w-full bg-slate-200 flex justify-between py-4 items-center px-5">
+      <div className="font-mono uppercase text-2xl">zsimarketing</div>
+      <ul className="">
+        {menuItems.map((menu) => (
+          <li className="inline-block mr-2" key={menu.title}>
+            {menu.title}
+          </li>
+        ))}
+      </ul>
+      <div className="flex gap-3">
+        <span className="flex items-center gap-2 hover:text-white">
+          <FaPhoneAlt />
+          <a href="tel:1-833-711-4606" className=" ">
+            1-833-711-4606
+          </a>
+        </span>
+        <button className="border-2 border-black px-3 py-2 rounded-md font-semibold hover:bg-black hover:text-white">
+          Book Now Pay Later
+        </button>
+        <button className="border-2 border-black px-3 py-2 rounded-md font-semibold hover:bg-black hover:text-white">
+          Account Login
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
---
+++ src/config/site.ts
@@ -0,0 +1,10 @@
+export const menuItems = [
+  { title: "Home", location: "/" },
+  { title: "About", location: "/about" },
+  { title: "Services", location: "/services" },
+  { title: "Fleet", location: "/fleet" },
+  { title: "Get A Quote", location: "/get-a-quote" },
+  { title: "Contact", location: "/contact" },
+  { title: "Rate", location: "/rate" },
+  { title: "Driver Login", location: "/driver-login" },
+];
